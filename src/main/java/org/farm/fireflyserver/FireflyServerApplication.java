44f702ea7959dc1dde77b79e5efa9abc804ad9a3
hysong4u <hayeon_song@naver.com>
2025-08-05 15:24:22 +0900

    chore : 스프링 프로젝트 기본 패키지

---
+++ src/main/java/org/farm/fireflyserver/FireflyServerApplication.java
@@ -0,0 +1,12 @@
+package org.farm.fireflyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireflyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireflyServerApplication.class, args);
+    }
+}
---
+++ src/test/java/org/farm/fireflyserver/FireflyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.farm.fireflyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireflyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
