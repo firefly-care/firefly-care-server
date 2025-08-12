63bebc1f87a75aec2943bd85429edf1f57628cdc
hysong4u <hayeon_song@naver.com>
2025-08-12 09:39:26 +0900

    feat : #2 healthChecker 구현

--- src/main/java/org/farm/fireflyserver/common/util/HealthCheckController.java
+++ src/main/java/org/farm/fireflyserver/common/util/HealthCheckController.java
@@ -1,16 +1,19 @@
 package org.farm.fireflyserver.common.util;
 
+import lombok.RequiredArgsConstructor;
 import org.farm.fireflyserver.common.response.BaseResponse;
 import org.farm.fireflyserver.common.response.SuccessCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HealthCheckController {
 
     @GetMapping
-    public ResponseEntity<BaseResponse<?>> healthCheck() {
-        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, "Firefly Server Health Check OK"));
+    BaseResponse<String> healthChecker(){
+        String message = " Hello Firefly Server!";
+        return BaseResponse.of(SuccessCode.OK, message);
     }
+
 }
