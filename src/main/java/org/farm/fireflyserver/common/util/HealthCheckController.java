22d3073939b3d4175bac47b5446cc7d8baf37515
hysong4u <hayeon_song@naver.com>
2025-08-12 10:44:46 +0900

    feat : #2 임시 토큰 발급 API 구현

--- src/main/java/org/farm/fireflyserver/common/util/HealthCheckController.java
+++ src/main/java/org/farm/fireflyserver/common/util/HealthCheckController.java
@@ -6,7 +6,6 @@ import org.farm.fireflyserver.common.response.SuccessCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 public class HealthCheckController {
 
