2cf66c2b3945129404fa5fa39f2ca0ad3f78de19
peter001019 <peter001019@naver.com>
2025-08-12 15:45:58 +0900

    docs: DB 테이블 주석 추가

--- src/main/java/org/farm/fireflyserver/care/domain/Account.java
+++ src/main/java/org/farm/fireflyserver/care/domain/Account.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
 
 @Entity
 @Getter(AccessLevel.PROTECTED)
