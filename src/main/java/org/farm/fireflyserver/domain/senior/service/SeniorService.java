d7cebf0992d9f953a68e3d5c5ccfbf20e02ee5be
hysong4u <hayeon_song@naver.com>
2025-08-19 14:38:12 +0900

    refactor : senior 구현체 인터페이스 분리

--- src/main/java/org/farm/fireflyserver/domain/senior/service/SeniorService.java
+++ src/main/java/org/farm/fireflyserver/domain/senior/service/SeniorService.java
@@ -1,25 +1,8 @@
 package org.farm.fireflyserver.domain.senior.service;
 
-import lombok.RequiredArgsConstructor;
-import org.farm.fireflyserver.domain.senior.web.mapper.SeniorMapper;
 import org.farm.fireflyserver.domain.senior.web.dto.request.RegisterSeniorDto;
-import org.farm.fireflyserver.domain.senior.persistence.entity.Senior;
-import org.farm.fireflyserver.domain.senior.persistence.repository.SeniorRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class SeniorService {
-
-    private final SeniorRepository seniorRepository;
-    private final SeniorMapper seniorMapper;
-
-    @Transactional
-    public void registerSenior(RegisterSeniorDto dto) {
-        Senior senior = seniorMapper.toEntity(dto);
-        seniorRepository.save(senior);
-    }
+public interface SeniorService {
+    void registerSenior(RegisterSeniorDto dto);
 
 }
--- src/main/java/org/farm/fireflyserver/domain/senior/web/mapper/SeniorMapper.java
+++ src/main/java/org/farm/fireflyserver/domain/senior/web/mapper/SeniorMapper.java
@@ -3,7 +3,6 @@ package org.farm.fireflyserver.domain.senior.web.mapper;
 import org.farm.fireflyserver.domain.senior.persistence.entity.Senior;
 import org.farm.fireflyserver.domain.senior.web.dto.request.RegisterSeniorDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface SeniorMapper {
