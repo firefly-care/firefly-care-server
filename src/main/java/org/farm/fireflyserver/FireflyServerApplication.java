a82f8eefd5b6cc911f0b353a93bcd7030bbb3905
hysong4u <hayeon_song@naver.com>
2025-08-27 17:11:39 +0900

    chore : #24 스케줄링 빈 추가

--- src/main/java/org/farm/fireflyserver/FireflyServerApplication.java
+++ src/main/java/org/farm/fireflyserver/FireflyServerApplication.java
@@ -3,7 +3,9 @@ package org.farm.fireflyserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class FireflyServerApplication {
