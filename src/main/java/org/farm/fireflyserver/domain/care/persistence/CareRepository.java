f007ad1c57149b0bfd91b430dbb55dc80d25ccf1
peter001019 <peter001019@naver.com>
2025-08-27 12:31:05 +0900

    fix : #28 불필요한 import 제거

--- src/main/java/org/farm/fireflyserver/domain/care/persistence/CareRepository.java
+++ src/main/java/org/farm/fireflyserver/domain/care/persistence/CareRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.time.YearMonth;
 import java.util.List;
 
 public interface CareRepository extends JpaRepository<Care, Long> {
