ccce7f0c4230b752c083054871b8932b3a1d5d09
hysong4u <hayeon_song@naver.com>
2025-09-06 22:04:45 +0900

    fix : #38 공통 시간 포맷 추가

--- src/main/java/org/farm/fireflyserver/domain/led/service/batch/LedItemReader.java
+++ src/main/java/org/farm/fireflyserver/domain/led/service/batch/LedItemReader.java
@@ -1,24 +1,22 @@
 package org.farm.fireflyserver.domain.led.service.batch;
 
-import org.farm.fireflyserver.domain.led.web.dto.response.LedDataLogDto;
 import org.farm.fireflyserver.domain.led.persistence.entity.SensorGbn;
-import org.springframework.batch.item.ItemReader;
+import org.farm.fireflyserver.domain.led.web.dto.response.LedDataLogDto;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 
 import javax.sql.DataSource;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-
+import java.util.Map;
 
 // 10분 이내 LED 센서 로그를 읽어오는 ItemReader
 public class LedItemReader {
 
-    public static ItemReader<LedDataLogDto> reader(DataSource ledDataSource) {
+    public static JdbcCursorItemReader<LedDataLogDto> reader(DataSource ledDataSource, Map<String, LocalDateTime> windowTimes) {
 
-        LocalDateTime now = LocalDateTime.now();
-
-        Timestamp start = Timestamp.valueOf(now.minusMinutes(10));
-        Timestamp end = Timestamp.valueOf(now);
+        Timestamp start = Timestamp.valueOf(windowTimes.get("windowStart"));
+        Timestamp now = Timestamp.valueOf(windowTimes.get("now"));
 
         return new JdbcCursorItemReaderBuilder<LedDataLogDto>()
                 .name("ledSensorLogReader")
@@ -27,13 +25,14 @@ public class LedItemReader {
                         "FROM t_led_sensor_log " +
                         "WHERE REG_DT >= ? AND REG_DT < ? " +
                         "ORDER BY REG_DT DESC")
-                .queryArguments(start, end)
-                .rowMapper((rs, rowNum) -> new LedDataLogDto(
-                        rs.getString("LED_MTCHN_SN"),
-                        SensorGbn.fromCode(rs.getString("LED_SENSOR_GBN")),
-                        rs.getTimestamp("REG_DT").toLocalDateTime(),
-                        null
-                ))
+                .queryArguments(start, now)
+                .rowMapper((rs, rowNum) -> {
+                    String ledMtchnSn = rs.getString("LED_MTCHN_SN");
+                    SensorGbn sensorGbn = SensorGbn.fromCode(rs.getString("LED_SENSOR_GBN"));
+                    LocalDateTime eventTime = rs.getTimestamp("REG_DT").toLocalDateTime();
+                    return new LedDataLogDto(ledMtchnSn, sensorGbn, eventTime,null);
+                }
+                )
                 .build();
     }
 }
